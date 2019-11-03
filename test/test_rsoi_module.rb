4e46255126de3f7614f2777b95ef9038810b0c11
alekseyl <leshchuk@gmail.com>
2019-11-03 16:19:28 +0300

    rails 6 now works without AS in select

--- test/test_rsoi_module.rb
+++ test/test_rsoi_module.rb
@@ -5,6 +5,8 @@ class RsoiModuleTest < ActiveSupport::TestCase
   test 'all kind of nested attributes' do
     assert_raise(NoMethodError) { WarGroup.first.warriors_count }
     assert( WarGroup.test_select('Uruk').where( coalition: 'nazguls & co' ).first.warriors_count == 1 )
+    assert( WarGroup.test_select_without_as('Uruk').where( coalition: 'nazguls & co' ).first.warriors_count == 1 )
+
     assert( !WarGroup.test_select('Uruk').order('combatants.strength').where( coalition: 'nazguls & co' ).first.id.nil?)
 
     assert( WarGroup.test_select('Frodo').where( coalition: 'fellowship & co' ).first.warriors_count == 1 )
